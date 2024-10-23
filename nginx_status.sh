a4b7d1a1bc7560e830558ff5052d916bf6906f5b
KhauanGB <khauangb1225@gmail.com>
2024-10-23 15:45:16 -0300

    Correções no script de monitoramento de Nginx

--- nginx_status.sh
+++ nginx_status.sh
@@ -3,7 +3,7 @@
 DATA=$(date "+%Y-%m-%d %H:%M:%S")
 
 if systemctl is-active --quiet nginx; then
-	echo "$DATA Nginx ONLINE - O serviço está rodando corretamente" > home/khauan/nginx_inline.txt
+	echo "$DATA Nginx ONLINE - O serviço está rodando corretamente" > /home/khauan/nginx_online.txt
 else
 	echo "$DATA Nginx OFFLINE - O serviço não está rodando corretamente" > /home/khauan/nginx_offline.txt
 fi
