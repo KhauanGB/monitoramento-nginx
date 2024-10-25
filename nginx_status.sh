ef1ff0694da57f5eac28ed463bb5dfef14981487
KhauanGB <khauangb1225@gmail.com>
2024-10-25 16:10:38 -0300

    Atualizando para salvar todos os logs nos arquivos .log

--- nginx_status.sh
+++ nginx_status.sh
@@ -3,7 +3,7 @@
 DATA=$(date "+%Y-%m-%d %H:%M:%S")
 
 if systemctl is-active --quiet nginx; then
-	echo "$DATA Nginx ONLINE - O serviço está rodando corretamente" > /home/khauan/nginx_online.txt
+	echo "$DATA Nginx ONLINE - O serviço está rodando corretamente" >> /home/khauan/nginx_online.log
 else
-	echo "$DATA Nginx OFFLINE - O serviço não está rodando corretamente" > /home/khauan/nginx_offline.txt
+	echo "$DATA Nginx OFFLINE - O serviço parou ou apresentou um problema" >> /home/khauan/nginx_offline.log
 fi
